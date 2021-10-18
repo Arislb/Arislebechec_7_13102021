e2875cca5d8ff1885cc23e6aad700e7e3e288354
Aris <arislebechec@gmail.com>
2021-10-18 16:54:04 +0200

    Accueil page, espace.

---
+++ frontend/vuegroupomania/src/components/Login.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="contenergeneral">
+        <div class="contener-log"></div>
+        <div class="contener">
+            
+            <div class="barre"></div>
+            <div class="space-log"></div>
+        </div>
+        <div class="contener-signup"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+.contenergeneral{
+    background-color: yellowgreen;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+}
+.contener-log{
+    background-color:red;
+    width: 35%;
+    height:100%;
+}
+.contener{
+    background-color: blue;
+    width: 30%;
+    height: 100%;
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: center;
+}
+
+.space-log{
+    background-color: yellow;
+    width: 30%;
+    height: 10%;
+}
+.barre{
+    background-color:black;
+    width: 5%;
+    height: 50%;
+}
+
+.contener-signup{
+    background-color: white;
+    width: 35%;
+    height: 100%;
+}
+</style>
--- frontend/vuegroupomania/src/router/index.js
+++ frontend/vuegroupomania/src/router/index.js
@@ -1,27 +1,43 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Accueil from "../views/Accueil.vue";
+import Sign from "../views/Sign.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
+
   {
-    path: '/about',
-    name: 'About',
+    path: "/accueil",
+    name: "Accueil",
+    component: Accueil,
+  },
+
+  {
+    path: "/Signup",
+    name: "Signup",
+    component: Sign,
+  },
+
+  {
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
