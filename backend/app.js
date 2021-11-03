65d29ea431150f72cfa6e4c80807a948a58a2b32
Aris <arislebechec@gmail.com>
2021-11-03 16:18:41 +0100

    Route Log/signup

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,46 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+//const path = require("path");
+
+//const helmet = require("helmet");
+//const hpp = require("hpp");
+
+//Import des routes utilisé
+//const stuffRoutes = require("./routes/stuff");
+const userRoutes = require("./routes/user");
 
 const app = express();
+
+// rajout a l'entête des headers pour autoriser tout le monde  d'utiliser l'API
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Sécurité
+//app.use(helmet());
+//app.use(hpp());
+
+//destination des images
+//app.use("/images", express.static(path.join(__dirname, "images")));
+
+//enregistrement des Applications utilisé
+app.use("/api/auth", userRoutes);
+//app.use("/api/sauces", stuffRoutes);
+
+app.use((req, res) => {
+  res.json({ message: "Votre requête a bien été reçue !" });
+});
 
 module.exports = app;
---
+++ backend/controllers/user.js
@@ -0,0 +1,50 @@
+const datab = require("../models/index");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+//logique d'engeristement signup et hashage du mot de passe.
+exports.signup = (req, res, next) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = datab.User.build({
+        email: req.body.email,
+        username: req.body.username,
+        password: hash,
+        isAdmin: 0,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
+exports.login = (req, res, next) => {
+  datab.User.findOne({
+    where: { email: req.body.email },
+  })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "Utilisateur non trouvé !" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
+          }
+
+          res.status(200).json({
+            userId: user.id,
+            token: jwt.sign({ userId: user.id }, "RANDOM_TOKEN_SECRET", {
+              expiresIn: "24h",
+            }),
+          });
+          console.log(valid);
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
