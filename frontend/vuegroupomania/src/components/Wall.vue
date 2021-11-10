df04fd639253b2577a9dbdc1c172f04187cb6588
Aris <arislebechec@gmail.com>
2021-11-10 15:55:14 +0100

    Zone de text wall et centrage

--- frontend/vuegroupomania/src/components/Wall.vue
+++ frontend/vuegroupomania/src/components/Wall.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>Bienvenue sur le mur {{ username }}</div>
+  <v-app>
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center dense">
+          <v-col cols="12" sm="8" md="4" lg="4">
+            <div>Bienvenue sur le mur {{ username }}</div>
+            <v-textarea
+              outlined
+              name="input-7-4"
+              label="Ecris un message"
+              value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+            ></v-textarea>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
